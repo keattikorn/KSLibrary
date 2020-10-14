6904298c240a28c6f3b02dd0fc89194134a28396
Keattikorn <keattikorn@gmail.com>
2020-10-15 00:21:49 +0700

    restructure

---
+++ Assets/KS/AI/FSM/Samples/SimpleCharacterAI/Scripts/Patrol.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace KS.AI.FSM.SampleFSMs.SimpleCharacterAI
+{
+    public class Patrol : State
+    {
+        private SimpleAIFSM simpleAIFSM;
+        
+        private int currentWaypointIdx = -1;
+
+        private NavMeshAgent agent;
+        private ThirdPersonCharacter thirdPersonCharacter;
+        private AICharacterControl aiCharacterControl;
+        
+        public Patrol(FiniteStateMachine fsm) : base(fsm)
+        {
+            simpleAIFSM = ((SimpleAIFSM) this.FSM);
+            
+            agent = ((SimpleAIFSM) this.FSM).Agent;
+            thirdPersonCharacter = ((SimpleAIFSM) this.FSM).ThirdPersonChar;
+            aiCharacterControl = ((SimpleAIFSM) this.FSM).AICharControl;
+
+            
+        }
+
+        public override void Enter()
+        {
+            float lastDist = Mathf.Infinity; // Store distance between NPC and waypoints.
+            
+            // Calculate closest waypoint by looping around each one and calculating the distance between the NPC and each waypoint.
+            for (int i = 0; i < simpleAIFSM.wayPoints.Count; i++)
+            {
+                GameObject thisWP = simpleAIFSM.wayPoints[i].gameObject;
+                float distance = Vector3.Distance(simpleAIFSM.Npc.transform.position, thisWP.transform.position);
+                if(distance < lastDist)
+                {
+                    currentWaypointIdx = i;
+                    lastDist = distance;
+
+                    simpleAIFSM.AICharControl.SetTarget(thisWP.transform);
+                }            
+            }
+            
+            //Proceed to the next stage of the FSM
+            base.Enter();
+        }
+
+        public override void Update()
+        {
+            if (aiCharacterControl.target != null)
+                agent.SetDestination(aiCharacterControl.target.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                //Move the agent
+                thirdPersonCharacter.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                //increase waypoint index
+                if (currentWaypointIdx >= simpleAIFSM.wayPoints.Count-1)
+                    currentWaypointIdx = 0;
+                else
+                    currentWaypointIdx++;
+
+                //Set target to the next waypoint
+                aiCharacterControl.SetTarget(simpleAIFSM.wayPoints[currentWaypointIdx]);
+                agent.SetDestination(aiCharacterControl.target.position);
+
+                //Stop the character movement
+                thirdPersonCharacter.Move(Vector3.zero, false, false);
+            }
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+    }
+}
---
+++ Assets/KS/AI/FSM/Samples/SimpleCharacterAI/Scripts/SimpleAIFSM.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace KS.AI.FSM.SampleFSMs.SimpleCharacterAI
+{
+    [RequireComponent(typeof(UnityEngine.Animator))]
+    [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+    public class SimpleAIFSM : FiniteStateMachine
+    {
+        public List<Transform> wayPoints;
+        
+        public GameObject Npc { get; set; }
+        public Animator Anim { get; set; }
+
+        [SerializeField] public Transform player;
+        public Transform Player
+        {
+            get { return player; }
+        }
+        public NavMeshAgent Agent { get; set; }
+
+        public ThirdPersonCharacter ThirdPersonChar { get; set; }
+        public AICharacterControl AICharControl { get; set; }
+        
+        private void Start()
+        {
+            //The start state
+            CurrentState = new SimpleCharacterAI.Idle(this);
+            
+            Npc = GetComponent<Transform>().gameObject;
+            
+            Anim = GetComponent<Animator>();
+            Agent = GetComponent<NavMeshAgent>();
+            
+            ThirdPersonChar = GetComponent<ThirdPersonCharacter>();
+            AICharControl = GetComponent<AICharacterControl>();
+        }
+    }
+
+    
+}
+
