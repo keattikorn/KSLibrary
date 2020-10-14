93265f3a919bf835ed7cd12434ceb2fed1003e2f
Keattikorn <keattikorn@gmail.com>
2020-10-14 23:28:23 +0700

    initial commit

---
+++ AI/FSM/SampleFSMs/SimpleCharacterAI/Idle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace KS.AI.FSM.SampleFSMs.SimpleCharacterAI
+{
+    public class Idle : State
+    {
+        public Idle(FiniteStateMachine fsm) : base(fsm)
+        {
+        }
+
+        public override void Enter()
+        {
+            //Proceed to the next stage of the FSM state
+            StateStage = StateEvent.UPDATE;
+        }
+
+        public override void Update()
+        {
+            if (Random.Range(0, 100) < 10)
+            {
+                this.FSM.NextState = new SimpleCharacterAI.Patrol(this.FSM);
+                this.StateStage = StateEvent.EXIT; // The next time 'Process' runs, the EXIT stage will run instead, which will then return the nextState.
+            }
+        }
+
+        public override void Exit()
+        {
+
+        }
+    }
+}
---
+++ AI/FSM/SampleFSMs/Template/FSMTemplate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace KS.AI.FSM.SampleFSMs.Template
+{
+    public class FSMTemplate : FiniteStateMachine
+    {
+        public GameObject NpcGameObject { get; set; }
+        
+        //TODO: delete this line
+        public string currentStateName = "";
+        
+        void Start()
+        {
+            //The start state, usually be the Idle state
+            CurrentState = new StateTemplate(this);
+            
+            NpcGameObject = GetComponent<Transform>().gameObject;
+        }
+        
+    }
+}
